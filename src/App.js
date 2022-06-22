cae9feaa769d9ae6193bff86442a2f758cfb19e0
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-06-22 15:53:21 +0100

    Generate lorem

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello world');
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
   };
   return (
     <section className="section-center">
@@ -25,14 +32,9 @@ function App() {
         </button>
       </form>
       <article className="lorem-text">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse facilis
-          deleniti.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse facilis
-          deleniti.
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
